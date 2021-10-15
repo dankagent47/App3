9875a0c92729744011d1241c2bd052f1a0e24a9a
dankagent47 <81704816+dankagent47@users.noreply.github.com>
2021-10-15 21:05:20 +0300

    Добавьте файлы проекта.

---
+++ App3/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace App3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxResult = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxResult
+            // 
+            this.textBoxResult.Location = new System.Drawing.Point(35, 36);
+            this.textBoxResult.Multiline = true;
+            this.textBoxResult.Name = "textBoxResult";
+            this.textBoxResult.ReadOnly = true;
+            this.textBoxResult.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxResult.Size = new System.Drawing.Size(487, 520);
+            this.textBoxResult.TabIndex = 23;
+            //this.textBoxResult.TextChanged += new System.EventHandler(this.textBoxResult_TextChanged);
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(231, 578);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(94, 29);
+            this.btnSubmit.TabIndex = 28;
+            this.btnSubmit.Text = "ПУСК";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(548, 631);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.textBoxResult);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxResult;
+        private System.Windows.Forms.Button btnSubmit;
+    }
+}
+
---
+++ App3/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace App3
+{
+    public partial class Form1 : Form
+    {
+        private double Factorial(int x)
+        {
+            return x <= 1 ? x : x * Factorial(x - 1);
+        }
+            
+        private double TaylorSeries(double x, int N) //should be SinTaylorSeries
+        {
+            double result = x;
+            for (int n = 1; n < N; n++)
+            {
+                result += Math.Pow(-1, n) * Math.Pow(x, 2 * n + 1) / this.Factorial(2 * n + 1);
+            }
+            return result;
+            
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.textBoxResult.Multiline = true;
+            this.textBoxResult.WordWrap = false;
+            this.textBoxResult.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+                        
+                int n = 16;
+                double x_n, x_k, h;
+                x_n = 0.1;
+                x_k = 1;
+                h = (x_k - x_n) / (double)n;
+
+                string header = String.Format("Лаб. раб. 3 N1 Ст. гр. 10702220 Григелевича С. Т.\r\n" +                    
+                    "N = {0}\r\n" +
+                    "H = {1}\r\n\r\n", 
+                    n, h);
+
+                string results = "";
+
+                for (double x_i = x_n; x_i <= x_k + h; x_i += h)
+                {
+                    double resultSin, resultSinTaylorSeries;
+                    resultSin = Math.Round(Math.Sin(x_i), 3);
+                    resultSinTaylorSeries = Math.Round(this.TaylorSeries(x_i, n), 3);
+                    string result = String.Format("при x = {0:N3}  Y(x) = {1:N3}  S(x) = {2:N3}\r\n", x_i, resultSin, resultSinTaylorSeries);
+                    results += result;
+                }
+
+                textBoxResult.Text = header + results;            
+        }
+        
+    }
+    
+}
